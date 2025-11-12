f95a0f7544d77187edba7535790b563992d0db3b
luanaraujo2 <luanzinho.ao05@gmail.com>
2025-11-11 21:59:39 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,33 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#define tamanho 100
+#include <string.h>
+#define TAMANHO 1000
 
-void cubo1(float L, float *area, float *volume);
+void contadorLetras(char texto[TAMANHO], int *Q1, int *Q2);
 
-void cubo1(float L, float *area, float *volume){
-    *area = 6 * pow(L, 2);
-    *volume = pow(L, 3);
+void contadorLetras(char texto[TAMANHO], int *Q1, int *Q2){
+    int i = 0;
+    *Q1 = 0;
+    *Q2 = 0;
+    while(texto[i] != '\0'){
+        if(texto[i] >= 'a' && texto[i] <= 'z'){
+            *Q1 = (*Q1) + 1;
+        }
+        else if(texto[i] >= 'A' && texto[i] <= 'Z'){
+            *Q2 = (*Q2) + 1;
+        }
+        i++;
+    }
 }
 
 int main()
 {
-    int i = 0;
-    float L, area, volume;
-    while(i < tamanho){
-        printf("Dados do Cubo %i:\n", i + 1);
-        printf("Valor do Lado(L > 0): "); scanf("%f", &L);
-        if (L <= 0){
-            printf("ERRO: Lado invalido. Tente novamente.\n");
+    char texto[TAMANHO];
+    int Q1, Q2;
+    do{
+        printf("Digite um texto qualquer: "); scanf("\n%[^\n]", texto);
+        if (strcmp(texto, "SAIR") == 0 ||
+            strcmp(texto, "sair") == 0){
+            printf("Fim do programa.\n");
         }
         else{
-            printf("Dados de saida: ");
-            cubo1(L, &area, &volume);
-            printf("Area: %.1f metros quadrados.\n", area);
-            printf("Volume: %.1f metros cubicos.\n", volume);
+            // REUSABILIDADE
+            contadorLetras(texto, &Q1, &Q2);
+            printf("Texto: %s \n", texto);
+            printf("Quantidade de Letras Maiusculas: %i\n", Q1);
+            printf("Quantidade de Letras Minusculas: %i\n\n", Q2);
         }
-        i++;
-    }
+    }while(strcmp(texto, "SAIR") != 0 &&
+            strcmp(texto, "sair") != 0);
+
 
     return 0;
 }
